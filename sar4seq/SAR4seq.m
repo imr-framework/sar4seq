ee3763b48cf56b0ee546e44a7c7c13ba5a4cd821
Sairam Geethanath <sairamgeethanath@gmail.com>
2020-09-28 11:22:27 -0400

    Updates to Siemens calculations of SAR

--- sar4seq/SAR4seq.m
+++ sar4seq/SAR4seq.m
@@ -31,7 +31,7 @@ addpath(genpath('.'));
 
 
 if(nargin < 1)
-    seq_path = './seq_files/160_tse500ms.seq';
+    seq_path = './seq_files/180_tse500ms.seq';
     system = mr.opts('MaxGrad', 32, 'GradUnit', 'mT/m', ...
     'MaxSlew', 130, 'SlewUnit', 'T/m/s', 'rfRingdownTime', 30e-6, ...
     'rfDeadTime', 100e-6);
@@ -42,14 +42,14 @@ elseif(nargin <2)
     'MaxSlew', 130, 'SlewUnit', 'T/m/s', 'rfRingdownTime', 30e-6, ...
     'rfDeadTime', 100e-6);
     seq=mr.Sequence(system);
-    Sample_weight = 40;% kg
+    Sample_weight = 10;% kg
 elseif(nargin <3)
-    Sample_weight = 40;% kg
+    Sample_weight = 10;% kg
 end
 
 
 %% Constants
-SiemensB1fact = 1.5;%need to explore this further - B1+ factor
+SiemensB1fact = 1.32;%need to explore this further - B1+ factor
 GEB1fact = 1.1725;% need to explore this further - B1+ factor
 
 Wbody_weight = 103.45; %kg - from Visible Human Male
@@ -127,15 +127,23 @@ RFwbg_tavg = sum(SARwbg_vec)./T_scan./SiemensB1fact;
 RFhg_tavg = sum(SARhg_vec)./T_scan./SiemensB1fact;
 disp(['Time averaged RF power-Siemens is - Body: ', num2str(RFwbg_tavg),'W &  Head: ', num2str(RFhg_tavg), 'W']);
 
+SARwbg = max(SARwbg_vec); %W/kg but Siemens reports W/lb
+SARhg = max(SARhg_vec);
+
+Sample_head_weight = (Head_weight/Wbody_weight)*Sample_weight;
+
+SARwbg_predSiemens = SARwbg.* sqrt(Wbody_weight/Sample_weight)/2;
+SARhg_predSiemens = SARhg.* sqrt(Head_weight/Sample_head_weight)/2;
 
 
+% SARwbg_predSiemens = SARwbg_predSiemens/2.20462; %Siemens reports W/lb
+disp(['Predicted SAR-Siemens is - Body:', num2str(SARwbg_predSiemens), 'W/kg &  Head: ',num2str(SARhg_predSiemens), 'W/kg'])
 %% SAR whole body - match GE data
-SARwbg = max(SARwbg_vec);
-SARwbg_pred = SARwbg.* sqrt(Wbody_weight/Sample_weight).*GEB1fact;
-disp(['Predicted SAR-GE is ', num2str(SARwbg_pred), 'W/kg'])
+SARwbg_predGE = SARwbg.* sqrt(Wbody_weight/Sample_weight).*GEB1fact;
+disp(['Predicted SAR-GE is ', num2str(SARwbg_predGE), 'W/kg'])
 
  %% Check for each instant of the time averaged SAR with appropriate time limits
- if(sum(SARwbg_pred > TenSecThresh_wbg))
+ if(sum(SARwbg_predGE > TenSecThresh_wbg))
          error('Pulse sequence exceeding 10 second Global SAR limits, increase TR');
  end
 
